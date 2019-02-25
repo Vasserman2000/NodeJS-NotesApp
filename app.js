629340747510730dfe1248004e8cd5742e70ef99
vasserman2000 <vasserman2000@gmail.com>
2019-02-25 18:03:33 +0200

    npm try new package

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const fs = require('fs');
 const os = require('os');
 const myName = os.userInfo().username;
 const notes = require('./notes.js');
+const _ = require('lodash');
 
-fs.appendFileSync('greetings.txt', `Hello ${myName}! You are ${notes.age}`);
+// fs.appendFileSync('greetings.txt', `Hello ${myName}! You are ${notes.age}`);
 
-string: res = notes.addNote();
+// string: res = notes.addNote();
 
-console.log(res);
+// console.log(res);
 
-console.log(notes.add(9, -2));
+// console.log(notes.add(9, -2));
+
+console.log(_.isString(true));
+console.log(_.isString('Elisha'));
+
+var filteredArray = _.uniq(['Elisha', 1, 'Elisha', 1, 2, 3, 4]);
+console.log(filteredArray);
