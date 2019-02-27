0e7bae8d44525a04ab6c2020676008f91745f9fa
Vasserman2000 <vasserman2000@gmail.com>
2019-02-27 11:09:46 +0200

    Passing arguments to the app from the terminal

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const myName = os.userInfo().username;
-const notes = require('./notes.js');
 const _ = require('lodash');
 
-// fs.appendFileSync('greetings.txt', `Hello ${myName}! You are ${notes.age}`);
+const notes = require('./notes.js');
 
-// string: res = notes.addNote();
-
-// console.log(res);
-
-// console.log(notes.add(9, -2));
-
-console.log(_.isString(true));
-console.log(_.isString('Elisha'));
-
-var filteredArray = _.uniq(['Elisha', 1, 'Elisha', 1, 2, 3, 4]);
-console.log(filteredArray);
+var command = process.argv[2];
+ 
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Command not recognized');
+}
