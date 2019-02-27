53be939d8cb3d4dc08131f71310a6b392bb79810
Vasserman2000 <vasserman2000@gmail.com>
2019-02-27 22:54:11 +0200

    yargs package with console input|

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ const notes = require('./notes.js');
 
 var command = process.argv[2];
  
-if (command === 'add') {
-    console.log('Adding new note');
-} else if (command === 'list') {
-    console.log('Listing all notes');
-} else if (command === 'read') {
-    console.log('Reading note');
-} else if (command === 'remove') {
-    console.log('Removing note');
-} else {
-    console.log('Command not recognized');
-}
+// if (command === 'add') {
+//     console.log('Adding new note');
+// } else if (command === 'list') {
+//     console.log('Listing all notes');
+// } else if (command === 'read') {
+//     console.log('Reading note');
+// } else if (command === 'remove') {
+//     console.log('Removing note');
+// } else {
+//     console.log('Command not recognized');
+// }
+
+const yargs = require('yargs').argv;
+
+console.log(yargs);
+console.log(yargs.name + ' year: ' + yargs.year);
