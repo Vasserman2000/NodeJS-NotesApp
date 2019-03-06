1c93edf57d4799695f67c4baf468899ca7dc94c6
Vasserman2000 <vasserman2000@gmail.com>
2019-03-06 23:54:16 +0200

    yargs commands added

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ const notes = require('./notes.js');
 const argv = yargs.argv;
 var command = argv._[0];
 
+//yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!');
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note');
+    }
+});
+
+console.log(yargs.argv);
+
+
  
 if (command === 'add') {
     notes.addNote(argv.title, argv.body);
